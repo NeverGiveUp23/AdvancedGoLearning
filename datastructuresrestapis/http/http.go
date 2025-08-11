56e806093450f903ecec2ed26e7e3967bf5fdd07
felix <felixvjr0@gmail.com>
2025-08-10 23:49:17 -0400

    Adding and readjusting the http call to github by creating functions

--- datastructuresrestapis/http/http.go
+++ datastructuresrestapis/http/http.go
@@ -3,46 +3,48 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 // given github login, return name of public repos
 // make http req, then passing json needed
 
-
-
 func main() {
-	response , err := http.Get("https://api.github.com/users/nevergiveup23")
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
 	// always check the status is okay
-	if response.StatusCode != http.StatusOK {
-		fmt.Printf("ERROR: bad status - %s\n", response.Status)
+	fmt.Println(userInfo("nevergiveup23"))
+	// io.Copy -> copies from the reader to the writer which is the Stdout
+	// io.Copy just reads the incoming data from the request body (response.Body) in this case and sends somewhere else (os.Stdout) in this case
+
+}
+// userInfo() returns the name and number of public_repos from github api
+func userInfo(login string) (string, int, error) {
+	url := "https://api.github.com/users/" + login
+
+	res, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
 	}
 
-	// getting the content type 
-	ctype := response.Header.Get("Content-Type")
-	fmt.Println("Content-Type:", ctype)
+	if res.StatusCode != http.StatusOK {
+		return "", 0, fmt.Errorf("%q - bad status: %s", url, res.Status)
+	}
+	return parseResponse(res.Body)
+}
+
+func parseResponse(r io.Reader) (string, int, error) {
 	
 	var reply struct {
 		Name        string `json:"name"`
 		NumRepos int    `json:"public_repos"`
 	}
 
-	decoder := json.NewDecoder(response.Body)
+	decoder := json.NewDecoder(r)
 	if err := decoder.Decode(&reply); err != nil {
 		fmt.Println("ERROR: ", err)
-		return
+		return "", 0, err
 	}
-	fmt.Println(reply.Name, reply.NumRepos)
-
-
-	// io.Copy -> copies from the reader to the writer which is the Stdout
-	// io.Copy just reads the incoming data from the request body (response.Body) in this case and sends somewhere else (os.Stdout) in this case
-
+	return reply.Name, reply.NumRepos, nil
 }
 
 /* JSON <-> Go
@@ -52,7 +54,6 @@ func main() {
 	number <-> float, float32, int, int32, int8 ..., uint, uint8 ...
 	array <-> []T. []any
 	object <-> map[string]any, struct
-	
 
 	encodingjson api
 	JSON <-> []byte -> go: Unmarshal
